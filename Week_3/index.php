8eab2798810373592156ec649003a259867b1582
khuynnn <huynhuynkhanh@gmail.com>
2025-09-30 18:38:06 +0700

    update

---
+++ Week_3/index.php
@@ -0,0 +1,13 @@
+<?php
+include("func/func.php");
+$a =5;
+$b =6;
+$c=4;
+echo("Number a is: ".$a);
+echo("<br>Number b is: ".$b);
+echo("<br>Sum of a and b is: ".sum($a, $b));
+echo("<br>Number c is: ".$c);
+echo("<br>The smallest number is: ".minOfThree($a, $b, $c));
+
+
+?>
