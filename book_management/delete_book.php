aa7554bda1b999282aae10cd676160ccb6d40c86
khuynnn <huynhuynkhanh@gmail.com>
2025-12-05 17:59:16 +0700

    update

--- book_management/delete_book.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("connection.php");
-$id = $_GET["id"];
-
-mysqli_query($link, "DELETE FROM books WHERE id=$id");
-header("Location: books.php");
-exit();
-?>
--- book_management/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="footer">
-    <p>© 2025 Book Management System</p>
-</div>
